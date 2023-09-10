f6ed9a37cd24a0e3bfb583412ece315a48c049e6
Anatoliy Kirichenko <anatoliykirichenko01@gmail.com>
2023-09-10 18:29:27 +0700

    added useState react hook, event listeners for green card and red card quantity in inputs, finished general css styling of components

--- card-game/src/components/cardGame.jsx
+++ card-game/src/components/cardGame.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React,{useState} from "react";
+import './styles.css';
 export const CardGame = () => {
+    const [greenCardState, setGreenCardState] =useState(0);
+    const [redCardState, setRedCardState] = useState(0);
+
+    //🟩🟥
+
+    const setGreenCardValue = (event) => {
+        setGreenCardState(event.target.value);
+    }
+
+    const setRedCardValue = (event) => {
+        setRedCardState(event.target.value);
+    }
+
     return (
-        <h1>Hello there!</h1>
+        <div className="cardGame-wrapper">
+            <h1>Карточная игра</h1>
+            <p>Введите количество зёлёных и красных карт, а приложение определит, будет ли данная игра интересной</p>
+            <form className="cardGame-form">
+                <label>Зелёные карты<input className="cardGame-element" type="number" name="greenCard" value={greenCardState} onChange={setGreenCardValue} min="0" required></input></label>
+                <label>Красные карты<input className="cardGame-element" type="number" name="redCard" value={redCardState} onChange={setRedCardValue} min="0" required></input></label>
+                <button className="cardGame-element" type="submit">Отправить</button>
+                <div className="cardGame-wrapper">
+                    <output placeholder="123"></output>
+                </div>
+            </form>
+        </div>
     )
 
 }
