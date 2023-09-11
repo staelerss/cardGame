3d7b91c363620630cfe67eb87d1d293efb7ebb40
Anatoliy Kirichenko <anatoliykirichenko01@gmail.com>
2023-09-12 01:47:23 +0700

    Added useState hook for result output (currently not working)

--- card-game/src/components/cardGame.jsx
+++ card-game/src/components/cardGame.jsx
@@ -1,18 +1,14 @@
 import React,{useState} from "react";
 import './styles.css';
 export const CardGame = () => {
-    const [greenCardState, setGreenCardState] =useState(0);
-    const [redCardState, setRedCardState] = useState(0);
+    const [greenCardState, setGreenCardState] =useState(1);
+    const [redCardState, setRedCardState] = useState(1);
+    const [resultState, setResultState] = useState("🟥🟩");
 
-    let result = [];
+    let max = Math.max(greenCardState,redCardState);
+    let min = Math.min(greenCardState,redCardState);
 
-    for (let i = 0; i < greenCardState; i++) {
-        result.push("🟩");
-    }
-    for (let i = 0; i < redCardState; i++) {
-        result.push("🟥");
-    }
-    //🟩🟥
+
 
     const setGreenCardValue = (event) => {
         setGreenCardState(event.target.value);
@@ -20,16 +16,19 @@ export const CardGame = () => {
     const setRedCardValue = (event) => {
         setRedCardState(event.target.value);
     }
+    const setResultValue = (event) => {
+        setResultState(event.target.value);
+    }
 
     return (
         <div className="cardGame-wrapper">
             <h1>Карточная игра</h1>
             <p>Введите количество зёлёных и красных карт, а приложение определит, будет ли данная игра интересной</p>
             <form className="cardGame-form">
-                <label>Зелёные карты<input className="cardGame-element" type="number" name="greenCard" value={greenCardState} onChange={setGreenCardValue} min="0"></input></label>
-                <label>Красные карты<input className="cardGame-element" type="number" name="redCard" value={redCardState} onChange={setRedCardValue} min="0"></input></label>
+                <label>Зелёные карты<input className="cardGame-element" type="number" name="greenCard" value={greenCardState} onChange={setGreenCardValue} min="1"></input></label>
+                <label>Красные карты<input className="cardGame-element" type="number" name="redCard" value={redCardState} onChange={setRedCardValue} min="1"></input></label>
                 <div className="cardGame-wrapper">
-                    <output name="result">{result}</output>
+                    <output name="result" onChange={setResultValue}>{resultState}</output>
                 </div>
             </form>
         </div>
