75b6d74ee25da47c3c0c1f209e4c8968754aa889
Anatoliy Kirichenko <anatoliykirichenko01@gmail.com>
2023-09-11 17:36:55 +0700

    Added dynamic update of card's amount output

--- card-game/src/components/cardGame.jsx
+++ card-game/src/components/cardGame.jsx
@@ -4,12 +4,19 @@ export const CardGame = () => {
     const [greenCardState, setGreenCardState] =useState(0);
     const [redCardState, setRedCardState] = useState(0);
 
+    let result = [];
+
+    for (let i = 0; i < greenCardState; i++) {
+        result.push("🟩");
+    }
+    for (let i = 0; i < redCardState; i++) {
+        result.push("🟥");
+    }
     //🟩🟥
 
     const setGreenCardValue = (event) => {
         setGreenCardState(event.target.value);
     }
-
     const setRedCardValue = (event) => {
         setRedCardState(event.target.value);
     }
@@ -19,11 +26,10 @@ export const CardGame = () => {
             <h1>Карточная игра</h1>
             <p>Введите количество зёлёных и красных карт, а приложение определит, будет ли данная игра интересной</p>
             <form className="cardGame-form">
-                <label>Зелёные карты<input className="cardGame-element" type="number" name="greenCard" value={greenCardState} onChange={setGreenCardValue} min="0" required></input></label>
-                <label>Красные карты<input className="cardGame-element" type="number" name="redCard" value={redCardState} onChange={setRedCardValue} min="0" required></input></label>
-                <button className="cardGame-element" type="submit">Отправить</button>
+                <label>Зелёные карты<input className="cardGame-element" type="number" name="greenCard" value={greenCardState} onChange={setGreenCardValue} min="0"></input></label>
+                <label>Красные карты<input className="cardGame-element" type="number" name="redCard" value={redCardState} onChange={setRedCardValue} min="0"></input></label>
                 <div className="cardGame-wrapper">
-                    <output placeholder="123"></output>
+                    <output name="result">{result}</output>
                 </div>
             </form>
         </div>
